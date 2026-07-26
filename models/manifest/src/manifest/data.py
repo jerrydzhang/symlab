701097b9f4345349645a4da135970bb4e1fbe5ab
jerrydzhang <jerry.zhang.d@gmail.com>
2026-07-26 16:59:21 -0400

    created data loader and batcher

---
+++ models/manifest/src/manifest/data.py
@@ -0,0 +1,55 @@
+from typing import TypedDict
+
+import numpy as np
+import torch
+from symbolic import Evaluated
+
+from manifest.tokenizer import XValsTokenizer, PAD_ID
+
+from torch.nn.utils.rnn import pad_sequence
+
+
+class SRBatch(TypedDict):
+    data: torch.Tensor
+    tokens: torch.Tensor
+    num_values: torch.Tensor
+    data_mask: torch.Tensor
+    token_mask: torch.Tensor
+
+
+def collate_fn(samples: list[Evaluated], tokenizer: XValsTokenizer) -> SRBatch:
+    tokens_list = []
+    num_values_list = []
+    data_list = []
+
+    for sample in samples:
+        tokens, num_values = tokenizer.encode(sample.expression)
+        tokens_list.append(torch.tensor(tokens, dtype=torch.long))
+        num_values_list.append(torch.tensor(num_values, dtype=torch.float))
+
+        combined_data = np.column_stack((sample.X, sample.y))
+        data_list.append(torch.from_numpy(combined_data).float())
+
+    padded_tokens = pad_sequence(tokens_list, batch_first=True, padding_value=PAD_ID)
+    padded_num_values = pad_sequence(
+        num_values_list, batch_first=True, padding_value=1.0
+    )
+    token_mask = padded_tokens != PAD_ID
+
+    data_lens = torch.tensor([d.shape[0] for d in data_list])
+    max_n = int(data_lens.max().item())
+    batch_size = len(data_list)
+    # n_features + target
+    D = data_list[0].shape[1]
+    data_padded = torch.zeros(batch_size, max_n, D)
+    for i, d in enumerate(data_list):
+        data_padded[i, : d.shape[0], :] = d
+    data_mask = torch.arange(max_n)[None, :] < data_lens[:, None]
+
+    return SRBatch(
+        data=data_padded,
+        tokens=padded_tokens,
+        num_values=padded_num_values,
+        data_mask=data_mask,
+        token_mask=token_mask,
+    )
--- notebooks/models/manifest/train.py
+++ notebooks/models/manifest/train.py
@@ -6,6 +6,56 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
+    import numpy as np
+
+    from symbolic import OperatorSet
+    from symbolic.transforms import split, add_noise
+    from symbolic.scoring import r2, complexity
+    from symbolic.generation import (
+        Pipeline,
+        RandomBinaryTree,
+        MantissaExponentConstants,
+        UniformSamplePoints,
+        is_valid,
+    )
+
+    from manifest.model import TransformerModel
+    from manifest.loss import compute_loss
+    from manifest.tokenizer import XValsTokenizer
+
+    return (
+        MantissaExponentConstants,
+        OperatorSet,
+        Pipeline,
+        RandomBinaryTree,
+        UniformSamplePoints,
+        is_valid,
+        np,
+    )
+
+
+@app.cell
+def _(np):
+    rng = np.random.default_rng(7)
+    return (rng,)
+
+
+@app.cell
+def _(
+    MantissaExponentConstants,
+    OperatorSet,
+    Pipeline,
+    RandomBinaryTree,
+    UniformSamplePoints,
+    is_valid,
+    rng,
+):
+    pipeline = (
+        Pipeline(RandomBinaryTree(OperatorSet.default(), rng=rng))
+        .then(MantissaExponentConstants(rng=rng))
+        .then(UniformSamplePoints(rng=rng))
+        .filter(is_valid())
+    )
     return
 
 
